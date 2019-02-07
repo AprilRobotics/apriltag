b706323138cc4c4fa52e22a388aa726168bcbfbe
Max Krogius <mkrogius@umich.edu>
2019-02-07 16:24:21 -0500

    Update default decimation factor to 2. This should be the most typical value used.

--- apriltag.c
+++ apriltag.c
@@ -342,7 +342,7 @@ apriltag_detector_t *apriltag_detector_create()
     apriltag_detector_t *td = (apriltag_detector_t*) calloc(1, sizeof(apriltag_detector_t));
 
     td->nthreads = 1;
-    td->quad_decimate = 1.0;
+    td->quad_decimate = 2.0;
     td->quad_sigma = 0.0;
 
     td->qtp.max_nmaxima = 10;
