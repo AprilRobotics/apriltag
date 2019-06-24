717fc8b85318ea60a0c2fba721f160a6ce823152
LV SETUP <setup@logivations.com>
2019-06-24 10:19:48 +0000

    custom 5x5 markers

---
+++ tag25h8.h
@@ -0,0 +1,15 @@
+#ifndef _TAG25H8
+#define _TAG25H8
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tag25h8_create();
+void tag25h8_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
