271d0c496231293d89003ab2406aa7d9f4787d83
nehildanis1 <nehil.danis@logivations.com>
2020-09-07 23:27:52 +0200

    new type standard41h11

---
+++ tagStandard41h11.h
@@ -0,0 +1,17 @@
+#ifndef _TAGStandard41H11
+#define _TAGStandard41H11
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tagStandard41h11_create();
+void tagStandard41h11_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
