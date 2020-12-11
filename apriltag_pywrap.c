8f2e2856813349d846e9270478fe197b72475ddc
nhryadil <37399936+nhryadil@users.noreply.github.com>
2020-12-11 17:29:11 +0200

    Update apriltag_pywrap.c

--- apriltag_pywrap.c
+++ apriltag_pywrap.c
@@ -8,6 +8,7 @@
 
 #include "apriltag.h"
 #include "tag36h11.h"
+#include "tag25h8.h"
 #include "tag25h9.h"
 #include "tag16h5.h"
 #include "tagCircle21h7.h"
@@ -19,6 +20,7 @@
 
 #define SUPPORTED_TAG_FAMILIES(_)           \
     _(tag36h11)                             \
+    _(tag25h8)                              \
     _(tag25h9)                              \
     _(tag16h5)                              \
     _(tagCircle21h7)                        \
