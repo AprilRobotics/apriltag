c632a91549774374eaf98374b7616ddfcf929012
yus <yusufziya95@hotmail.com>
2021-09-27 15:04:08 +0200

    New tag families can be used in Python

---
+++ tag36h10.h
@@ -0,0 +1,17 @@
+#ifndef _TAG36H10
+#define _TAG36H10
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tag36h10_create();
+void tag36h10_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
